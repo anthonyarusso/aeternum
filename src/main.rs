d3cebb9585a63da53c4c42080421a7354cfc1c1b
Anthony A Russo <aarusso.exe@outlook.com>
2020-12-19 22:31:30 -0800

    main.rs is now up-to-date with the Bevy Tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 fn add_people(commands: &mut Commands) {
     commands
     .spawn((Person, Name("Elaina Proctor".to_string())))
@@ -11,9 +7,31 @@ fn add_people(commands: &mut Commands) {
     .spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
+
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if the timer hasn't finished yet, we return
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+
     for name in query.iter() {
         println!("hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        // the reason we call from_seconds with the ture flag is to make the timer repeat itself
+        app
+            .add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+
     }
 }
 
@@ -23,9 +41,7 @@ struct Name(String);
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
+        .add_plugin(HelloPlugin)
         .run();
 
 }
