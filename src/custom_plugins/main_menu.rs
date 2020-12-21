9c85fecf2c5f3e8efa5512df4a828aa9ca3a8bf4
Anthony A Russo <aarusso.exe@outlook.com>
2020-12-20 23:02:32 -0800

    Began refactor of Main Menu functionality into a separate Rust Module and Bevy Plugin. To-Do: Finish MainMenuPlugin and FPSCounterPlugin.

--- src/custom_plugins/main_menu.rs
+++ src/custom_plugins/main_menu.rs
@@ -1,86 +1,13 @@
 use bevy::{
-    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     prelude::*,
 };
 
-// import 'custom_plugins' as a module
-mod custom_plugins;
+pub struct MainMenuPlugin;
 
-/// This example illustrates how to create text and update it in a system. It displays the current FPS in the upper left hand corner.
-fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .init_resource::<ButtonMaterials>()
-        .init_resource::<BackgroundMaterials>()
-        .add_startup_system(setup.system())
-        .add_system(text_update_system.system())
-        .add_system(button_system.system())
-        .run();
-}
-
-struct ButtonMaterials {
-    normal: Handle<ColorMaterial>,
-    hovered: Handle<ColorMaterial>,
-    pressed: Handle<ColorMaterial>,
-}
-
-impl FromResources for ButtonMaterials {
-    fn from_resources(resources: &Resources) -> Self {
-        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
-        ButtonMaterials {
-            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
-            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
-        }
-    }
-}
-
-struct BackgroundMaterials {
-    alphadark: Handle<ColorMaterial>,
-}
-
-impl FromResources for BackgroundMaterials {
-    fn from_resources(resources: &Resources) -> Self {
-        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
-        BackgroundMaterials {
-            alphadark: materials.add(Color::rgba(0.3, 0.3, 0.3, 0.7).into()),
-        }
-    }
-}
-
-fn button_system (
-    button_materials: Res<ButtonMaterials>,
-    mut interaction_query: Query<
-        (&Interaction, &mut Handle<ColorMaterial>, &Children),
-        (Mutated<Interaction>, With<Button>),
-    >,
-) {
-    for (interaction, mut material, _children) in interaction_query.iter_mut() {
-        match *interaction {
-            Interaction::Clicked => {
-                *material = button_materials.pressed.clone();
-            }
-            Interaction::Hovered => {
-                *material = button_materials.hovered.clone();
-            }
-            Interaction::None => {
-                *material = button_materials.normal.clone();
-            }
-        }
-    }
-}
-
-// A unit struct to help identify the FPS UI component, since there may be many Text components
-struct FpsText;
-
-fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
-    for mut text in query.iter_mut() {
-        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
-            if let Some(average) = fps.average() {
-                text.value = format!("FPS: {:.2}", average);
-            }
-        }
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app
+            .add_startup_system(setup.system());
     }
 }
 
@@ -90,9 +17,7 @@ fn setup(
     button_materials: Res<ButtonMaterials>,
     background_materials: Res<BackgroundMaterials>,
 ) {
-    commands
-        // 2d camera
-        .spawn(CameraUiBundle::default());
+    let button_texts: [&str; 4] = ["New Game", "Load Game", "Credits", "Settings"];
     commands
         .spawn(NodeBundle {
             style: Style {
@@ -219,39 +144,34 @@ fn setup(
                 });
             });
         });
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Px(100.0), Val::Px(30.0)),
-                justify_content: JustifyContent::FlexStart,
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    top: Val::Px(5.0),
-                    left: Val::Px(5.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            material: background_materials.alphadark.clone(), 
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn(TextBundle {
-            style: Style {
-                padding: Rect::all(Val::Auto),
-                ..Default::default()
-            },
-            text: Text {
-                value: "FPS:".to_string(),
-                font: asset_server.load("fonts/SourceSansPro-Regular.ttf"),
-                style: TextStyle {
-                    font_size: 24.0,
-                    color: Color::YELLOW,
-                    ..Default::default()
-                },
-            },
-            ..Default::default()
-            })
-            .with(FpsText);
-        });
+}
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+    pressed: Handle<ColorMaterial>,
+}
+
+impl FromResources for ButtonMaterials {
+    fn from_resources(resources: &Resources) -> Self {
+        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
+        ButtonMaterials {
+            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
+            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+        }
+    }
+}
+
+struct BackgroundMaterials {
+    alphadark: Handle<ColorMaterial>,
+}
+
+impl FromResources for BackgroundMaterials {
+    fn from_resources(resources: &Resources) -> Self {
+        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
+        BackgroundMaterials {
+            alphadark: materials.add(Color::rgba(0.3, 0.3, 0.3, 0.7).into()),
+        }
+    }
 }
