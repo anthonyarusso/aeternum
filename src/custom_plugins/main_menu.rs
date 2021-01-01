91360cde3cc01a4de899f2524b1c0742e4c35340
Anthony A Russo <aarusso.exe@outlook.com>
2021-01-01 00:19:40 -0800

    Created a separate 'Main' and 'Pause' menu and routed buttons accordingly. Game now opens to the main menu.

--- src/custom_plugins/main_menu.rs
+++ src/custom_plugins/main_menu.rs
@@ -13,14 +13,17 @@ impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app
             .add_plugin(audio::AudioPlugin)
-            .add_resource(State::new(AppState::Menu))
+            .add_resource(State::new(AppState::MainMenu))
             .init_resource::<materials::ButtonMaterials>()
             .init_resource::<GlobalCounters>()
             .init_resource::<MenuOptions>()
             .add_stage_after(stage::UPDATE, STAGE, StateStage::<AppState>::default())
-            .on_state_enter(STAGE, AppState::Menu, setup_menu.system())
-            .on_state_update(STAGE, AppState::Menu, menu.system())
-            .on_state_exit(STAGE, AppState::Menu, cleanup_menu.system())
+            .on_state_enter(STAGE, AppState::MainMenu, setup_menu.system())
+            .on_state_update(STAGE, AppState::MainMenu, menu.system())
+            .on_state_exit(STAGE, AppState::MainMenu, cleanup_menu.system())
+            .on_state_enter(STAGE, AppState::PauseMenu, setup_pause_menu.system())
+            .on_state_update(STAGE, AppState::PauseMenu, pause_menu.system())
+            .on_state_exit(STAGE, AppState::PauseMenu, cleanup_pause_menu.system())
             .on_state_enter(STAGE, AppState::SettingsMenu, setup_settings_menu.system())
             .on_state_update(STAGE, AppState::SettingsMenu, settings_menu.system())
             .on_state_exit(STAGE, AppState::SettingsMenu, cleanup_settings_menu.system())           .on_state_enter(STAGE, AppState::InGame, setup_game.system())
@@ -34,13 +37,15 @@ const STAGE: &str = "app_state";
 
 #[derive(Clone)]
 enum AppState {
-    Menu,
+    MainMenu,
+    PauseMenu,
     SettingsMenu,
     InGame,
 }
 
 pub struct MenuOptions {
-    main: [&'static str; 4],
+    main: [&'static str; 5],
+    pause: [&'static str; 4],
     settings: [&'static str; 6],
 }
 
@@ -48,6 +53,13 @@ impl FromResources for MenuOptions {
     fn from_resources(_resources: &Resources) -> Self {
         MenuOptions {
             main: [
+                "Play Game",
+                "New Game",
+                "Settings",
+                "Credits",
+                "Exit",
+            ],
+            pause: [
                 "Resume Game",
                 "Settings",
                 "Main Menu",
@@ -60,7 +72,7 @@ impl FromResources for MenuOptions {
                 "Graphics",
                 "Language",
                 "Previous Menu",
-            ]
+            ],
         }
     }
 }
@@ -77,16 +89,74 @@ impl FromResources for GlobalCounters {
     }
 }
 
+struct MainMenuEntity {
+    main_entity: Entity,
+}
+
 struct SettingsMenuEntity {
     main_entity: Entity,
 }
 
-struct MainMenuEntity {
+struct PauseMenuEntity {
     main_entity: Entity,
 }
 
 struct GameData {
     sprite_entity: Entity,
+}
+
+fn setup_pause_menu(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    button_materials: Res<materials::ButtonMaterials>,
+    menu_options: Res<MenuOptions>,
+) {
+    commands
+        .spawn(CameraUiBundle::default());
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(80.0), Val::Percent(80.0)),
+                margin: Rect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::ColumnReverse,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            for i in 0..menu_options.pause.len() {
+                parent.spawn(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(250.0), Val::Px(65.0)),
+                        margin: Rect::all(Val::Px(25.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items:AlignItems::Center,
+                        ..Default::default()
+                    },
+                    material: button_materials.normal.clone(),
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            value: menu_options.pause[i].to_string(),
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            style: TextStyle {
+                                font_size: 40.0,
+                                color: Color::rgb(1.0, 1.0, 1.0),
+                                ..Default::default()
+                            },
+                        },
+                        ..Default::default()
+                    });
+                }); 
+            } // end for-loop
+        });
+    commands.insert_resource(PauseMenuEntity {
+        main_entity: commands.current_entity().unwrap(),
+    });
 }
 
 fn setup_menu(
@@ -225,10 +295,54 @@ fn menu(
                 if text.value == menu_options.main[0].to_string() {
                     state.set_next(AppState::InGame).unwrap();
                 } else if text.value == menu_options.main[1].to_string() {
-                    state.set_next(AppState::SettingsMenu).unwrap()
+                    state.set_next(AppState::InGame).unwrap();
                 } else if text.value == menu_options.main[2].to_string() {
-                    println!("Display Main Menu");
+                    state.set_next(AppState::SettingsMenu).unwrap()
                 } else if text.value == menu_options.main[3].to_string() {
+                    println!("Made by ur mom, lol");
+                } else if text.value == menu_options.main[4].to_string() {
+                    app_exit_events.send(AppExit);
+                } else {
+                    println!("Main Menu Text");
+                }
+            }
+            Interaction::Hovered => {
+                *material = button_materials.hovered.clone();
+            }
+            Interaction::None => {
+                *material = button_materials.normal.clone();
+            }
+        }
+    }
+}
+
+fn pause_menu(
+    mut state: ResMut<State<AppState>>,
+    button_materials: Res<materials::ButtonMaterials>,
+    mut interaction_query: Query<
+        (&Interaction, &mut Handle<ColorMaterial>, &Children),
+        (Mutated<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut app_exit_events: ResMut<Events<AppExit>>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+    menu_options: Res<MenuOptions>,
+) {
+    let button_click = asset_server.load("audio/sounds/click.mp3");
+    for (interaction, mut material, children) in interaction_query.iter_mut() {
+        let text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                *material = button_materials.pressed.clone();
+                audio.play(button_click.clone());
+                if text.value == menu_options.pause[0].to_string() {
+                    state.set_next(AppState::InGame).unwrap();
+                } else if text.value == menu_options.pause[1].to_string() {
+                    state.set_next(AppState::SettingsMenu).unwrap()
+                } else if text.value == menu_options.pause[2].to_string() {
+                    state.set_next(AppState::MainMenu).unwrap()
+                } else if text.value == menu_options.pause[3].to_string() {
                     app_exit_events.send(AppExit);
                 } else {
                     println!("Mama mia!");
@@ -266,7 +380,7 @@ fn settings_menu(
                 if text.value == menu_options.settings[0].to_string() {
                     println!("Display Accessibility Settings");
                 } else if text.value == menu_options.settings[5].to_string() {
-                    state.set_next(AppState::Menu).unwrap();
+                    state.set_next(AppState::PauseMenu).unwrap();
                 } else {
                     println!("Clicky clack");
                 }
@@ -282,6 +396,10 @@ fn settings_menu(
 }
 
 fn cleanup_settings_menu(commands: &mut Commands, menu_data: Res<SettingsMenuEntity>) {
+    commands.despawn_recursive(menu_data.main_entity);
+}
+
+fn cleanup_pause_menu(commands: &mut Commands, menu_data: Res<PauseMenuEntity>) {
     commands.despawn_recursive(menu_data.main_entity);
 }
 
@@ -333,7 +451,7 @@ fn movement(
             direction.y -= 1.0;
         }
         if input.pressed(KeyCode::Escape) {
-           state.set_next(AppState::Menu).unwrap(); 
+           state.set_next(AppState::PauseMenu).unwrap(); 
         }
 
         if direction != Vec3::default() {
