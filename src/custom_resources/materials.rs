481658901b5d48f4b28de3199690bc12829fa186
Anthony A Russo <aarusso.exe@outlook.com>
2021-01-01 16:37:43 -0800

    Added new BackgroundMaterials to custom_resources.rs.

--- src/custom_resources/materials.rs
+++ src/custom_resources/materials.rs
@@ -4,16 +4,6 @@ use bevy::{
     prelude::*
 };
 
-/*
-pub struct CustomMaterialsPlugin;
-
-impl Plugin for CustomMaterialsPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app
-            .init_resource::<ButtonMaterials>();
-    }
-}
-*/
 pub struct ButtonMaterials {
     pub normal: Handle<ColorMaterial>,
     pub hovered: Handle<ColorMaterial>,
@@ -27,6 +17,25 @@ impl FromResources for ButtonMaterials {
             normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
             hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
             pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+        }
+    }
+}
+
+pub struct BackgroundMaterials {
+    pub alpha: Handle<ColorMaterial>,
+    pub alpha_red: Handle<ColorMaterial>,
+    pub image_main: Handle<ColorMaterial>,
+}
+
+impl FromResources for BackgroundMaterials {
+    fn from_resources(resources: &Resources) -> Self {
+        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
+        let asset_server = resources.get::<AssetServer>().unwrap();
+        BackgroundMaterials {
+            alpha: materials.add(Color::NONE.into()),
+            alpha_red: materials.add(Color::rgba(255.0, 0.0, 0.0, 0.6).into()),
+            image_main: materials
+                .add(asset_server.load("images/main_menu/ancient_rome.png").into()),
         }
     }
 }
